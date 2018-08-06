e91be61859b10b81e8e4dd1b7b79b212df8fbf0b
fruity4pie <fruity4pie@gmail.com>
2018-08-06 20:19:01 +0300

    added mobmenu

--- app/js/main.js
+++ app/js/main.js
@@ -23,13 +23,64 @@ $(document).ready(function() {
     }
   });
 
-  function stopCarousel(...args) {
+  function showCarousels(...args) {
     for(let i = 0; i < args.length; i++) {
-      i = $(`${args[i]}`);
-      i.trigger('destroy.owl.carousel');
-      i.removeClass('owl-carousel');
+      let c = $(`${args[i]}`).owlCarousel({
+        loop: true,
+        responsiveClass: true,
+        nav: false,
+        dots: true,
+        autoHeight:true,
+        responsive:{
+          0:{
+            items:1,
+          }
+        }
+      })
+      c.addClass('owl-carousel');
     }
   }
+
+  function stopCarousel(...args) {
+    for(let i = 0; i < args.length; i++) {
+      let c = $(`${args[i]}`);
+      c.trigger('destroy.owl.carousel');
+      c.removeClass('owl-carousel');
+    }
+  }
+
+  //MobileMenu
+  let mobHamb = $('.m-burg-btn');
+  let mobHambClose = $('.m-burg__h > span');
+
+  mobHamb.on('click', function(){
+    $(this).parent().find('.m-burg__wrap').addClass('active');
+  })
+  mobHambClose.on('click', function() {
+    mobHamb.parent().find('.m-burg__wrap').removeClass('active');
+  })
+
+  //MobileTabs
+  let tabsAnch = $('.mob-tabs-a a');
+  let prev;
+  tabsAnch.on('click', function(e){
+    e.preventDefault();
+    $(this).parent().parent().parent().find('a').removeClass('active');
+    $(this).toggleClass('active');
+    $(this).parent().parent().parent().find('.mob-tabs-c').has('active') ? $(this).toggleClass('active').parent().siblings('.mob-tabs-c').toggleClass('active') : $(this).parent().siblings('.mob-tabs-c').toggleClass('active');
+  })
+
+  let mapAnch = $('.mob-tabs2-a a');
+
+  mapAnch.on('click', function(e){
+    e.preventDefault();
+    if($(this).parent().hasClass('active')) {
+      $(this).parent().removeClass('active').siblings().removeClass('active');
+    } else {
+      $(this).parent().parent().parent().find('.mob-tabs2-a').removeClass('active').siblings().removeClass('active');
+      $(this).parent().toggleClass('active').siblings().toggleClass('active');
+    }
+  })
 
   $('.owlc1').owlCarousel({
     loop:true,
@@ -47,17 +98,6 @@ $(document).ready(function() {
         items:4,
       },
     }
-  })
-
-  //MobileTabs
-  let tabsAnch = $('.mob-tabs-a a');
-  let prev;
-
-  tabsAnch.on('click', function(e){
-    e.preventDefault();
-    $(this).parent().parent().parent().find('a').removeClass('active');
-    $(this).toggleClass('active');
-    $(this).parent().parent().parent().find('.mob-tabs-c').has('active') ? $(this).toggleClass('active').parent().siblings('.mob-tabs-c').toggleClass('active') : $(this).parent().siblings('.mob-tabs-c').toggleClass('active');
   })
 
   $('.owlc2').owlCarousel({
@@ -105,23 +145,5 @@ $(document).ready(function() {
     e.preventDefault();
     popUp.removeClass('active');
   })
-
-  function showCarousels(...args) {
-    for(let i = 0; i < args.length; i++) {
-      i = $(`${args[i]}`).owlCarousel({
-        loop: true,
-        responsiveClass: true,
-        nav: false,
-        dots: true,
-        autoHeight:true,
-        responsive:{
-          0:{
-            items:1,
-          }
-        }
-      })
-      i.addClass('owl-carousel');
-    }
-  }
 
 })
